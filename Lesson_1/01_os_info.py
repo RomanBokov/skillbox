5e2d66be3c23f20af2505e3c4684846992a596c9
Roman.Bokov <Nazgyl-92@mail.ru>
2020-05-12 23:56:53 +0300

    Update info file close

--- Lesson_1/01_os_info.py
+++ Lesson_1/01_os_info.py
@@ -1,9 +1,9 @@
 #TODO запустить этот скрипт и закоментировать результат  его работы
 
 import platform
-import  sys
+import sys
 
 info = ''.format(platform.uname(),sys.version,platform.architecture())
 print(info)
 with open('os_info.txt','w') as ff:
-    ff.writable(info)
+    ff.write(info)
